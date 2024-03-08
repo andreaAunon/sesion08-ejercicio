1380e3e877527d06b73b874a60aeba3c40ae6388
Andrea Auñón Antúnez <andrea.aunon@babelgroup.com>
2024-03-08 11:05:39 +0100

    proyecto añadido

---
+++ src/main/java/es/babel/demo/Sesion08EjercicioApplication.java
@@ -0,0 +1,13 @@
+package es.babel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sesion08EjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sesion08EjercicioApplication.class, args);
+	}
+
+}
